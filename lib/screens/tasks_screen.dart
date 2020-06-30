7d46518e568fd38879af782ceae3b09f1ee1dde9
Ernest-Lee <ernestrlee@yahoo.com>
2020-06-29 22:44:27 -0700

    Refactored code to use Provider package for state management.

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -17,12 +17,7 @@ class TasksScreen extends StatelessWidget {
           showModalBottomSheet(context: context,isScrollControlled: true,
               builder: (context) => SingleChildScrollView(
                   padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-                  child: AddTaskScreen( (newTaskTitle) {
-//                    setState(() {
-//                      tasks.add(Task(name: newTaskTitle));
-//                    });
-                    Navigator.pop(context);
-                  }),
+                  child: AddTaskScreen(),
               ),
           );
         },
