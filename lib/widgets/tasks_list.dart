a609c221d6a5d50bec7e0bbdcfa5a6a1eb1bdf4b
Ernest-Lee <ernestrlee@yahoo.com>
2020-06-29 23:37:42 -0700

    Added gesture detector for long press to delete task item.

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -12,12 +12,17 @@ class TasksList extends StatelessWidget {
         return ListView.builder(
           itemBuilder: (context, index) {
             final task = taskData.tasks[index];
-            return TaskTile(
-              taskTitle: task.name,
-              isChecked: task.isDone,
-              checkboxCallback: (bool checkboxState) {
-                taskData.updateTask(task);
+            return GestureDetector(
+              onLongPress: (){
+                taskData.deleteTask(index);
               },
+              child: TaskTile(
+                taskTitle: task.name,
+                isChecked: task.isDone,
+                checkboxCallback: (bool checkboxState) {
+                  taskData.updateTask(task);
+                },
+              ),
             );
           },
           itemCount: taskData.taskCount,
