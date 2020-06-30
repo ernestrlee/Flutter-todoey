fb1a94cfcffcc5d3099d63b8bfbe9b83c6285b0c
Ernest-Lee <ernestrlee@yahoo.com>
2020-06-29 22:42:08 -0700

    Added method to add tasks to task list. Made task list private. Added getter for unmodifiable task list.

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:todoeyflutter/models/task.dart';
+import 'dart:collection';
 
 class TaskData extends ChangeNotifier{
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Buy milk'),
     Task(name: 'Buy eggs'),
     Task(name: 'Buy bread'),
@@ -11,7 +12,16 @@ class TaskData extends ChangeNotifier{
   ];
 
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  void addNewTask(String task){
+    _tasks.add(Task(name: task));
+    notifyListeners();
   }
 
 }
